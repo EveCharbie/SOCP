84c7776f0ec74c21106c49c660463e2707cc9569
Charbie <eve.charbonneau.1@umontreal.ca>
2026-01-21 11:48:26 +0100

    Analysis plot

--- main_run_analysis.py
+++ main_run_analysis.py
@@ -1,1 +1,33 @@
-# TODO
+import matplotlib.pyplot as plt
+import numpy as np
+import casadi as cas
+import pickle
+
+
+# --- Plot the results for the ObstacleAvoidance problem
+
+with open(
+        "/home/charbie/Documents/Programmation/SOCP/results/"
+        "ObstacleAvoidance_DirectCollocationPolynomial_MeanAndCovariance_DVG_1p0e-06_2026-01-20-17-02_not_robust.pkl",
+        "rb",
+) as f:
+    data_DCP_MAC = pickle.load(f)
+    n_shooting = data_DCP_MAC["difference_between_means"].shape[0] - 1
+
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+axs[0].plot(np.linspace(0, n_shooting, n_shooting+1), np.zeros((n_shooting+1, )), "--k")
+axs[0].plot(data_DCP_MAC["difference_between_means"], '-r', label="DCP + MAC (Gillis)")
+axs[0].set_title(r"$|\bar{x}_{opt} - \bar{x}_{sim}|$")
+axs[0].set_xlabel("Shooting node")
+axs[0].set_ylabel("Difference")
+
+axs[1].plot(np.linspace(0, n_shooting, n_shooting+1), np.zeros((n_shooting+1)), "--k")
+axs[1].plot(data_DCP_MAC["difference_between_covs"], '-r', label="DCP + MAC (Gillis)")
+axs[1].set_title(r"$|Det(P_{opt}) - Det(P_{sim})|$")
+axs[1].set_xlabel("Shooting node")
+
+axs[1].legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.savefig("obstacle_avoidance_analysis.png", dpi=300)
+plt.show()
