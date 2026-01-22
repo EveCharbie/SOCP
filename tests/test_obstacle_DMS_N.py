c30d311c1e25cedbcf74055bf607dbb01e199e1e
Charbie <eve.charbonneau.1@umontreal.ca>
2026-01-22 12:05:04 +0100

    Added a test for DMS - Noise

--- tests/test_obstacle_DMS_N.py
+++ tests/test_obstacle_DMS_N.py
@@ -4,18 +4,19 @@ import casadi as cas
 
 from socp import (
     ObstacleAvoidance,
-    DirectCollocationPolynomial,
-    MeanAndCovariance,
+    DirectMultipleShooting,
+    NoiseDiscretization,
     prepare_ocp,
     solve_ocp,
 )
 from socp.utils import is_semi_definite_positive
 
 
-def test_solve_DC_MAC():
+def test_solve_DMS_N():
 
-    dynamics_transcription = DirectCollocationPolynomial()
-    discretization_method = MeanAndCovariance(dynamics_transcription, with_cholesky=False, with_helper_matrix=True)
+    # DirectMultipleShooting - NoiseDiscretization -> OK :D
+    dynamics_transcription = DirectMultipleShooting()
+    discretization_method = NoiseDiscretization(dynamics_transcription)
 
     # --- Run the problem a first time without robustification of the constraint --- #
     ocp_example = ObstacleAvoidance(is_robustified=False)
@@ -28,13 +29,14 @@ def test_solve_DC_MAC():
     )
 
     # Solve the problem
-    w_opt, solver, grad_f_func, grad_g_func = solve_ocp(
+    w_opt, solver, grad_f_func, grad_g_func, save_path = solve_ocp(
         ocp,
         ocp_example=ocp_example,
         hessian_approximation="exact",  # or "limited-memory",
         linear_solver="ma57",
         pre_optim_plot=False,
         show_online_optim=False,
+        save_path_suffix="not_robust",
     )
 
     # --- Run the problem a second time robustified --- #
@@ -50,32 +52,33 @@ def test_solve_DC_MAC():
     ocp["w0"] = cas.DM(w_opt)
 
     # Solve the problem
-    w_opt, solver, grad_f_func, grad_g_func = solve_ocp(
+    w_opt, solver, grad_f_func, grad_g_func, save_path = solve_ocp(
         ocp,
         ocp_example=ocp_example,
         hessian_approximation="exact",  # or "limited-memory",
         linear_solver="ma57",
         pre_optim_plot=False,
         show_online_optim=False,
+        save_path_suffix="robustified",
     )
 
     # --- Test the results --- #
     nb_variables = ocp["w"].shape[0]
-    assert nb_variables == 5701
+    assert nb_variables == 1721
     nb_constraints = ocp["g"].shape[0]
-    assert nb_constraints == 5705
+    assert nb_constraints == 2437
     nb_non_zero_elem_in_grad_f = grad_f_func(ocp["w"]).nnz()
-    assert nb_non_zero_elem_in_grad_f == 5701
+    assert nb_non_zero_elem_in_grad_f == 1721
     nb_non_zero_elem_in_grad_g = grad_g_func(ocp["w"]).nnz()
-    assert nb_non_zero_elem_in_grad_g == 67431
+    assert nb_non_zero_elem_in_grad_g == 15050
     nb_iterations = solver.stats()["iter_count"]
-    npt.assert_equal(nb_iterations, 270)
+    npt.assert_equal(nb_iterations, 918)
 
     cost_function = cas.Function("cost_function", [ocp["w"]], [ocp["j"]])
     optimal_cost = float(cost_function(w_opt))
-    npt.assert_almost_equal(optimal_cost, 8.33022993497717, decimal=5)
+    npt.assert_almost_equal(optimal_cost, 1.6897254628504117, decimal=5)
 
-    npt.assert_almost_equal(np.sum(w_opt), 386.97682860321925, decimal=5)
+    npt.assert_almost_equal(np.sum(w_opt), 496.45680267939565, decimal=5)
 
     variable_opt = ocp["discretization_method"].Variables(
         ocp["ocp_example"].n_shooting,
@@ -88,24 +91,3 @@ def test_solve_DC_MAC():
     )
     variable_opt.set_from_vector(w_opt, only_has_symbolics=True)
 
-    for i_node in range(ocp["n_shooting"]):
-        cov_matrix_0 = variable_opt.get_cov_matrix(i_node)
-        cov_matrix_1 = variable_opt.get_cov_matrix(i_node + 1)
-        m_matrix = variable_opt.get_m_matrix(i_node)
-        noises_numerical = np.diag(np.array([1, 1]))
-        dGdx, dGdz, dGdw, dFdz = ocp["dynamics_transcription"].jacobian_funcs(
-            variable_opt.get_time(),
-            variable_opt.get_states(i_node),
-            variable_opt.get_collocation_points(i_node),
-            variable_opt.get_controls(i_node),
-            np.array([1, 1]),
-        )
-
-        assert is_semi_definite_positive(cov_matrix_0)
-        assert is_semi_definite_positive(cov_matrix_1)
-
-        cov_integrated = m_matrix @ (dGdx @ cov_matrix_0 @ dGdx.T + dGdw @ noises_numerical @ dGdw.T) @ m_matrix.T
-        npt.assert_array_less(np.max(np.abs(cov_matrix_1 - cov_integrated)), 1e-5)
-
-        m_constraint = dFdz.T - dGdz.T @ m_matrix.T
-        npt.assert_array_less(np.max(np.abs(m_constraint)), 1e-5)
