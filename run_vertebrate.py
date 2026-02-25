1e22a7935963b48ec4f1a320818dbd247439b267
Charbie <eve.charbonneau.1@umontreal.ca>
2026-02-25 15:25:57 +0100

    reduced RAM for biorbd functions

--- run_vertebrate.py
+++ run_vertebrate.py
@@ -40,7 +40,7 @@ def run_vertebrate(
         ocp,
         ocp_example=ocp_example,
         hessian_approximation="exact",  # or "limited-memory",
-        linear_solver="mumps",  # TODO: change back to ma57
+        linear_solver="ma57",  # TODO: change back to ma57
         pre_optim_plot=False,
         show_online_optim=False,
         save_path_suffix="",
--- socp/models/biorbd_model.py
+++ socp/models/biorbd_model.py
@@ -1,3 +1,4 @@
+from functools import wraps
 
 import casadi as cas
 import numpy as np
@@ -7,12 +8,45 @@ import pyorerun
 from .model_abstract import ModelAbstract
 
 
+
+def cache_function(method):
+    """Decorator to cache CasADi functions automatically"""
+
+    def make_hashable(value):
+        """
+        Transforms non-hashable objects (dicts, and lists) into hashable objects (tuple)
+        """
+        if isinstance(value, list):
+            return tuple(make_hashable(v) for v in value)
+        elif isinstance(value, dict):
+            return tuple(sorted((k, make_hashable(v)) for k, v in value.items()))
+        elif isinstance(value, set):
+            return frozenset(make_hashable(v) for v in value)
+        return value
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Create a unique key based on the method name and arguments
+        key = method.__name__
+        if key in self._cached_functions.keys():
+            return self._cached_functions[key]
+
+        # Call the original function to create the CasADi function
+        casadi_fun = method(self, *args, **kwargs)
+
+        # Store in the cache
+        self._cached_functions[key] = casadi_fun
+        return casadi_fun
+
+    return wrapper
+
 class BiorbdModel(ModelAbstract):
 
     def __init__(self, nb_random: int, model_name: str):
 
         super().__init__(nb_random=nb_random)
 
+        self._cached_functions = {}
         self.biorbd_model = biorbd.Model(f"socp/models/{model_name}.bioMod")
         self.nb_q = self.biorbd_model.nbQ()
 
@@ -20,6 +54,7 @@ class BiorbdModel(ModelAbstract):
     def name_dof(self):
         return [m.to_string() for m in self.biorbd_model.nameDof()]
 
+    @cache_function
     def forward_dynamics_biorbd(
             self,
             q: cas.SX | cas.DM | np.ndarray,
@@ -38,6 +73,7 @@ class BiorbdModel(ModelAbstract):
         )
         return fd_func(q, qdot, tau)
 
+    @cache_function
     def lagrangian_biorbd(
         self,
         q: cas.SX | cas.DM | np.ndarray,
@@ -54,6 +90,7 @@ class BiorbdModel(ModelAbstract):
         )
         return lagrangian_func(q, qdot)
 
+    @cache_function
     def momentum_biorbd(
         self,
         q: cas.SX | cas.DM | np.ndarray,
