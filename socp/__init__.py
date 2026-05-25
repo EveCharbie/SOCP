1e3bebc3dbad25eba26252f8fb00df55d5ea81fe
eve-mac <eve.charbonneau.1@umontreal.ca>
2026-05-25 16:02:28 -0400

    added some imports

--- socp/__init__.py
+++ socp/__init__.py
@@ -1,6 +1,7 @@
 from .analysis.save_results import save_results
 from .analysis.estimate_covariance import estimate_covariance
 
+from .examples.example_abstract import ExampleAbstract  # For inheritance
 from .examples.arm_reaching import ArmReaching
 from .examples.cart_pole import CartPole
 from .examples.obstacle_avoidance import ObstacleAvoidance
@@ -8,6 +9,9 @@ from .examples.somersault import Somersault
 from .examples.squat import Squat
 from .examples.vertebrate import Vertebrate
 from .examples.vertebrate_arm import VertebrateArm
+
+from .models.model_abstract import ModelAbstract  # For inheritance
+from .models.biorbd_model import BiorbdModel
 
 from .transcriptions.direct_multiple_shooting import DirectMultipleShooting
 from .transcriptions.direct_collocation_trapezoidal import DirectCollocationTrapezoidal
